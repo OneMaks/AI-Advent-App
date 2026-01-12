50b39c6adbd45c7052bad5a2fdc99a94f01b2094
OneMaks <66749193+OneMaks@users.noreply.github.com>
2026-01-12 23:04:37 +0300

    init template project commit

---
+++ AI Advent App/composeApp/src/androidMain/kotlin/ru/makscorp/project/Platform.kt
@@ -0,0 +1,9 @@
+package ru.makscorp.project
+
+import android.os.Build
+
+class AndroidPlatform {
+    val name: String = "Android ${Build.VERSION.SDK_INT}"
+}
+
+fun getPlatform() = AndroidPlatform()
